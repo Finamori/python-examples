6140a81793110bd627b73a84e3f237683bdb87e0
Paulo Macedo <paulo.macedo@iterative.com.br>
2021-04-04 22:29:49 -0300

    primeiros arquivos

---
+++ basic/basic_data_types.py
@@ -0,0 +1,6 @@
+print(type(10)) ## int
+print(type(4.2)) ##float
+print(type(1.8e308)) ##Floating-Point Representation
+print(type(2+3j)) ##Complex Numbers
+print(type("String")) ##String
+print(type(True)) ##Boolean 
---
+++ basic/variable.py
@@ -0,0 +1,16 @@
+n = 300
+print(n)
+a = b = c = 400
+print(a, b, c)
+
+###
+
+var = 23.5
+print(var)
+23.5
+
+var = "Now I'm a string"
+print(var)
+
+### 
+#  for = "string" ### SyntaxError: invalid syntax -> Reserved Words (Keywords)
